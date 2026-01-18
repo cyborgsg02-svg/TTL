d0fe4bd656b915e69c812b881d36140de92fd2a0
cyborgsg02-svg <cyborgsg02@gmail.com>
2026-01-18 17:29:27 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -14,7 +14,7 @@ export function score(rank, percent, minPercent) {
     if (rank > 100) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
+    if (rank > 25 && percent < 50) {
         return 0;
     }
 
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 1000) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
